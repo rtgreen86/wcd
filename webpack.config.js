ed07b4d160ae890f7f4ad544e3978209a5938b76
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2022-07-21 22:16:17 +0300

    bundle main process

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,32 @@
 /* eslint-env node */
 
 const path = require('path');
+const nodeExternals = require("webpack-node-externals");
 
-module.exports = {
+const common = {
+  mode: 'production'
+};
+
+const main = {
+  target: "electron-main",
+  externals: [nodeExternals()],
+  entry: './src/main/main.js',
+  output: {
+    filename: 'main.app.js',
+    path: path.resolve(__dirname, 'app')
+  }
+};
+
+const renderer = {
   target: 'electron-renderer',
   entry: './src/renderer/index.js',
   output: {
     filename: 'renderer.app.js',
     path: path.resolve(__dirname, 'app')
   }
-}
+};
+
+module.exports = [
+  Object.assign({}, common, main),
+  Object.assign({}, common, renderer)
+];
