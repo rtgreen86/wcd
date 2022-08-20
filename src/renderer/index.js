0312d76bb21663d9b16d08fb735c7a49510fa3a4
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2022-08-20 17:46:04 +0300

    fix React warning

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,13 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
-
-
-// Varian 2
-
-// import ReactDOM from 'react-dom/client';
-// const container = document.getElementById('app-container');
-// const root = ReactDOM.createRoot(container);
-// root.render(<App />);
+const container = document.getElementById('app');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
