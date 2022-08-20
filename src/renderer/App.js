655a454829d7dfb2c6383e2173a3fe1160faac78
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2022-08-20 19:38:41 +0300

    use reducer

--- src/renderer/App.js
+++ src/renderer/App.js
@@ -1,27 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 import { MonthCalendar, YearCalendar } from './features/calendar';
 import Status from './Status';
 
+const initialState = {
+  records: {},
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'add':
+      return {
+        records: {
+          ...state.records,
+          [action.payload]: 'blue'
+        }
+      };
+    default:
+      return state;
+  }
+}
 
 export default function App() {
   const [date, setDate] = useState('nothing clicked');
-
-  const [marks, setMarks] = useState({
-    "1986-01-03": 'green',
-    "1986-02-14": 'blue'
-  });
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const handleClick = (date) => {
     setDate(`Clicked: ${date}`);
-    setMarks({
-      ...marks,
-      [date]: 'blue'
+    dispatch({
+      type: 'add',
+      payload: date
     });
   }
 
   return (<>
     <Status message={date} />
-    <YearCalendar year={1986} firstDayOfWeek={1} marks={marks} onClick={handleClick} />
-    <MonthCalendar year={1986} month={1} firstDayOfWeek={1} marks={marks} onClick={handleClick} />
+    <YearCalendar year={1986} firstDayOfWeek={1} marks={state.records} onClick={handleClick} />
+    <MonthCalendar year={1986} month={1} firstDayOfWeek={1} marks={state.records} onClick={handleClick} />
   </>);
 }
