be2982117dd5546dfc564eaf799573f30b47dd9e
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2022-08-20 17:52:05 +0300

    support calendar days clicks

--- src/renderer/App.js
+++ src/renderer/App.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MonthCalendar, YearCalendar } from './features/calendar';
+import Status from './Status';
+
 
 export default function App() {
-  const marks = {
+  const [date, setDate] = useState('nothing clicked');
+
+  const [marks, setMarks] = useState({
     "1986-01-03": 'green',
     "1986-02-14": 'blue'
-  };
+  });
 
   const handleClick = (date) => {
-    console.log(`Clicked: ${date}`);
+    setDate(`Clicked: ${date}`);
+    setMarks({
+      ...marks,
+      [date]: 'blue'
+    });
   }
 
   return (<>
+    <Status message={date} />
     <YearCalendar year={1986} firstDayOfWeek={1} marks={marks} onClick={handleClick} />
     <MonthCalendar year={1986} month={1} firstDayOfWeek={1} marks={marks} onClick={handleClick} />
   </>);
---
+++ src/renderer/Status.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Status({ message }) {
+  return <div>{message}</div>;
+}
