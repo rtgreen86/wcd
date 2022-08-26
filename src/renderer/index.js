4b4dc5684728c2109f563c8e875149dfb8f701e4
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2022-08-26 20:34:08 +0300

    route parameters

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -4,6 +4,7 @@ import { MemoryRouter, Routes, Route } from "react-router-dom";
 import App from './App';
 import Expenses from "./routes/Expenses.jsx";
 import Invoices from "./routes/Invoices.jsx";
+import Invoice from "./routes/Invoice.jsx";
 
 
 const container = document.getElementById('app');
@@ -16,7 +17,25 @@ root.render(
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="expenses" element={<Expenses />} />
-        <Route path="invoices" element={<Invoices />} />
+        <Route path="invoices" element={<Invoices />}>
+          <Route path=":invoiceId" element={<Invoice />} />
+          <Route
+            index
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>Select an invoice</p>
+              </main>
+            }
+          />
+        </Route>
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
       </Route>
     </Routes>
   </MemoryRouter>
--- src/renderer/routes/Invoices.jsx
+++ src/renderer/routes/Invoices.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
+import { getInvoices } from "../data.js";
+
 
 export default function Invoices() {
+  const invoices = getInvoices();
+
   return (
-    <main style={{ padding: "1rem 0" }}>
-      <h2>Invoices</h2>
-      <Link to="/">Home</Link>
-    </main>
+    <div style={{ display: "flex" }}>
+      <nav
+        style={{
+          borderRight: "solid 1px",
+          padding: "1rem",
+        }}
+      >
+        {invoices.map((invoice) => (
+          <NavLink
+            style={({ isActive }) => {
+              return {
+                display: "block",
+                margin: "1rem 0",
+                color: isActive ? "red" : "",
+              };
+            }}
+            to={`/invoices/${invoice.number}`}
+            key={invoice.number}
+          >
+            {invoice.name}
+          </NavLink>
+        ))}
+      </nav>
+      <Outlet />
+    </div>
   );
 }
