0a9b108347933806759295b3d7827c9fa0993e41
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2022-08-28 13:58:05 +0300

    add devtools extension

--- src/main/main.js
+++ src/main/main.js
@@ -24,7 +24,7 @@ const createWindow = () => {
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.app.js')
-    }
+    },
   });
   win.loadFile('app/index.html');
 }
@@ -33,7 +33,18 @@ app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
 });
 
-app.whenReady().then(() => {
+app.whenReady().then(async () => {
+  // [Bug]: Electron sandbox_bundle.js script failed to run #32133
+  // see: https://github.com/electron/electron/issues/32133
+
+  // import installExtension, { REDUX_DEVTOOLS, REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
+  // installExtension(REACT_DEVELOPER_TOOLS)
+  //       .then((name) => console.log(`Added Extension:  ${name}`))
+  //       .catch((err) => console.log('An error occurred: ', err));
+  // installExtension(REDUX_DEVTOOLS)
+  //       .then((name) => console.log(`Added Extension:  ${name}`))
+  //       .catch((err) => console.log('An error occurred: ', err));
+
   fillAboutPanel();
   handleIpc();
   createWindow();
