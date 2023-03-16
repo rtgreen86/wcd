52d8749688f8445145e4addd1141c2aea8d38974
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2023-03-16 20:49:44 +0100

    add electron-forge

---
+++ forge.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  packagerConfig: {},
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    }
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-webpack',
+      config: {
+        mainConfig: './webpack.main.config.js',
+        renderer: {
+          config: './webpack.renderer.config.js',
+          entryPoints: [
+            {
+              html: './assets/index.html',
+              js: './src/render/renderer.js',
+              name: 'main_window',
+              preload: {
+                js: './src/preload/preload.js',
+              },
+            },
+          ],
+        },
+      },
+    },
+  ],
+};
