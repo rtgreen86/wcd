ffc0175a1d54d965e5535b3d2f5f249cc475817d
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2023-05-06 23:03:04 +0200

    Create Panel with menu

--- src/renderer/routes/Lock.jsx
+++ src/renderer/routes/Lock.jsx
@@ -44,7 +44,17 @@ export default function Lock() {
           <MenuItem align="right"><Button>Button</Button></MenuItem>
           <MenuItem align="right"><Button>Button</Button></MenuItem>
         </Menu>
-    </section>
+      </section>
+
+      <section>
+        <h3>Panel with menu</h3>
+        <Panel><Menu>
+          <MenuItem><Button>&lt;</Button><span>2023</span><Button>&gt;</Button></MenuItem>
+          <MenuItem><Button>Button</Button></MenuItem>
+          <MenuItem align="right"><Button>Button</Button></MenuItem>
+          <MenuItem align="right"><Button>Button</Button></MenuItem>
+        </Menu></Panel>
+      </section>
 
     </Main>
   );
