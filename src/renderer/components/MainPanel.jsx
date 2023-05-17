38e2203c69b53f17629bfed1dde5bea02ae2e1cf
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2023-05-17 22:17:14 +0200

    Create top panel

---
+++ src/renderer/components/MainPanel.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Panel, Button } from './UIKit';
+
+export default function MainPanel() {
+  return (
+    <Panel top>
+      <span>Span</span> <Button>Button</Button>
+    </Panel>
+  );
+}
--- src/renderer/components/MainScreen.jsx
+++ src/renderer/components/MainScreen.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
+import MainPanel from './MainPanel.jsx';
 
 export default function HomeScreen() {
   return (
     <>
-      <div>Main toolbar here</div>
+      <MainPanel />
       <Outlet />
     </>
   );
