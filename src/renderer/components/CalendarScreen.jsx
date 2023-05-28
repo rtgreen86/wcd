f450b476d433f5c682741c44bd3c2573db7097cc
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2023-05-29 00:20:24 +0200

    Cleanup code

--- src/renderer/components/CalendarScreen.jsx
+++ src/renderer/components/CalendarScreen.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
-import { useNavigate } from "react-router-dom";
-
-import { Button } from './UIKit';
-
 import { YearCalendar } from '../features/calendar';
 
 export default function CalendarScreen() {
-  const navigate = useNavigate();
   return (
     <main>
-      <section>
-        <Button onClick={() => navigate('/controls')}>Controls</Button>
-      </section>
-
       <section>
         <YearCalendar />
       </section>
