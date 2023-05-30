304d0333bcbe5a7ce454fae0a3682787623be131
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2023-05-30 21:40:34 +0200

    Cleanup: remove unused components

--- src/renderer/common/ui/index.js
+++
@@ -1,5 +0,0 @@
-export {default as Link} from './Link.jsx';
-export {default as Main} from './Main.jsx';
-export {default as Menu} from '../../components/Menu.jsx';
-export {default as MenuItem} from '../../components/MenuItem.jsx';
-export {default as Panel} from '../../components/Panel.jsx';
