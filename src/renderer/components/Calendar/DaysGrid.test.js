8d693f036c8b9b86e828a73a83f0894d689ff5e1
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2023-06-11 00:35:32 +0200

    Add snapshot test for DaysGrid

--- src/renderer/components/Calendar/DaysGrid.test.js
+++ src/renderer/components/Calendar/DaysGrid.test.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import DaysGrid from './DaysGrid';
 
-describe('DaysGrid', function () {
-  it('should create grid', async function () {
-    render(<DaysGrid year={2023} month={1} />);
-    await screen.findAllByText('1');
-    expect(screen.getAllByText('1')[0]).toHaveTextContent('1');
+describe('DaysGrid', () => {
+  describe.each([
+    [2000, 1],
+    [2000, 2],
+    [2000, 4],
+    [2001, 1],
+    [2001, 2],
+    [2001, 4],
+  ])('rendered with %i-%i', (year, month) => {
+    test('match snapshot', function () {
+      const { asFragment } = render(<table><tbody><DaysGrid year={year} month={month} /></tbody></table>);
+      expect(asFragment()).toMatchSnapshot();
+    });
   });
 });
