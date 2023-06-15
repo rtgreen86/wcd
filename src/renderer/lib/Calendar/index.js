bd5ce3dca6b9095c2694e5533e526c94a013ef37
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2023-06-15 21:26:57 +0200

    Refactor Calendar component

--- src/renderer/lib/Calendar/index.js
+++ src/renderer/lib/Calendar/index.js
@@ -1,2 +1,2 @@
-export { default as MonthCalendar } from './components/MonthCalendar';
-export { default as YearCalendar } from './components/YearCalendar';
+export { default as MonthCalendar } from './components/MonthCalendar.jsx';
+export { default as YearCalendar } from './components/YearCalendar.jsx';
