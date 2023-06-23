1ba9608e4a03a82ca975afe379f35cacd374f6b9
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2023-06-23 21:27:07 +0200

    Create storage in main thread

---
+++ __mocks__/electronMock.js
@@ -0,0 +1,5 @@
+import {jest} from '@jest/globals';
+
+export const app = {
+  getPath: jest.fn()
+}
--- jest.config.js
+++ jest.config.js
@@ -90,6 +90,7 @@ module.exports = {
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
     '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+    'electron': '<rootDir>/__mocks__/electronMock.js'
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
