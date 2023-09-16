aed02232c0a2d530de2f2e41a60dada4a858055d
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2023-09-16 11:15:35 +0200

    randomize iv

--- src/main/lib/fs-storage/files/EncryptedFile.ts
+++ src/main/lib/fs-storage/files/EncryptedFile.ts
@@ -2,12 +2,16 @@ import File from './File';
 import AbstractFile from './AbstractFile';
 import { FileType } from './FileType';
 import { createWriteStream, createReadStream } from 'node:fs';
-import { scrypt, randomFill, createCipheriv, scryptSync, createDecipheriv } from 'node:crypto';
+import { scrypt, createCipheriv, scryptSync, createDecipheriv, randomBytes } from 'node:crypto';
 import { pipeline } from 'node:stream';
+import { Buffer } from 'node:buffer';
 
 const algorithm = 'aes-192-cbc';
+
 const secret = 'Password used to generate key';
+
 const salt = 'salt';
+
 const keySize = 24;
 
 export default class EncryptedFile extends AbstractFile<string> {
@@ -25,54 +29,37 @@ export default class EncryptedFile extends AbstractFile<string> {
       scrypt(secret, salt, keySize, (err, key) => {
         if (err) return reject(err);
 
-        randomFill(new Uint8Array(16), (err, iv) => {
+        const iv = randomBytes(16);
+
+        const cipher = createCipheriv(algorithm, key, iv);
+
+        const output = createWriteStream(this.path);
+
+        output.write(iv);
+
+        pipeline(cipher, output, (err) => {
           if (err) return reject(err);
-
-          const iv2 = Buffer.alloc(16, 0);
-
-          const cipher = createCipheriv(algorithm, key, iv2);
-
-          const output = createWriteStream(this.path);
-
-
-          output.write(iv2);
-
-          pipeline(cipher, output, (err) => {
-
-
-            if (err) return reject(err);
-
-          });
-          cipher.on('end', () => reslove());
-
-          cipher.write(this.content);
-          cipher.end();
         });
+
+        cipher.on('end', () => reslove());
+        cipher.write(this.content);
+        cipher.end();
       });
     });
   }
 
   static load(path: string): Promise<File<string>> {
-    return new Promise((resolve, reject) => {
-
+    return new Promise((resolve) => {
       const input = createReadStream(path);
 
-
-
       input.once('readable', () => {
-
         const key = scryptSync(secret, salt, keySize);
-        // const iv = Buffer.alloc(16, 0);
         const iv = input.read(16);
         const decipher = createDecipheriv(algorithm, key, iv);
 
         let decrypted = '';
 
-
-
-
         decipher.on('readable', () => {
-          // reject('created iv');
           let chunk;
           while (null !== (chunk = decipher.read())) {
             decrypted += chunk.toString('utf8');
@@ -94,17 +81,7 @@ export default class EncryptedFile extends AbstractFile<string> {
         while (null !== (chunk = decipher.read())) {
           decrypted += chunk.toString('utf8');
         }
-
       });
-
-
-
-
     }) as Promise<File<string>>;
-
-
-
-
-
   }
 }
