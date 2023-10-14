2ffc0a9abfe886185e0c804dfc9cab05f6c808af
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2023-10-15 00:03:45 +0200

    Use global state and cleanup CalendarScreen

--- src/renderer/routes/CalendarScreen.tsx
+++ src/renderer/routes/CalendarScreen.tsx
@@ -1,148 +1,28 @@
-import React, { useReducer, useCallback } from 'react';
+import React from 'react';
 import { CalendarLocale, YearCalendar } from '../lib/Calendar';
 import MainPanel from '../components/MainPanel';
 import { marksToggle, useDispatch, useStore } from '../features/store';
 
-type Marks = {
-  [date: string]: string[]
-}
-
-type State = {
-  year: number,
-  marks: {
-    isLoading: boolean,
-    data: Marks
-  }
-}
-
-type YearIncrementAction = {
-  type: 'year/increment'
-}
-
-type YearDecrementAction = {
-  type: 'year/decrement'
-}
-
-type MarksLoadingAction = {
-  type: 'marks/loading'
-}
-
-type MarksLoadedAction = {
-  type: 'marks/loaded',
-  payload: Marks
-}
-
-type MarksSetAction = {
-  type: 'marks/set',
-  payload: Marks
-}
-
-type MarksUnsetAction = {
-  type: 'marks/unset',
-  payload: string
-}
-
-type MarksToggleAction = {
-  type: 'marks/toggle',
-  payload: {
-    date: string,
-    marks: Marks[keyof Marks]
-  }
-}
-
-type Action = YearIncrementAction | YearDecrementAction | MarksLoadingAction | MarksLoadedAction | MarksSetAction | MarksUnsetAction | MarksToggleAction;
-
-const initialArg: State = {
-  year: 0,
-  marks: {
-    isLoading: true,
-    data: {}
-  },
-};
-
-function init(initialArg: State) {
-  return {
-    ...initialArg,
-    year: new Date().getFullYear(),
-  };
-}
-
-function reducer(state: State, action: Action): State {
-  switch (action.type) {
-    case 'year/increment': return {
-      ...state,
-      year: state.year + 1
-    };
-
-    case 'year/decrement': return {
-      ...state,
-      year: state.year - 1
-    };
-
-    case 'marks/loading': return {
-      ...state,
-      marks: {
-        ...state.marks,
-        isLoading: true
-      }
-    };
-
-    case 'marks/loaded': return {
-      ...state,
-      marks: {
-        ...state.marks,
-        isLoading: false,
-        data: action.payload
-      }
-    };
-
-    case 'marks/set': return {
-      ...state,
-      marks: {
-        ...state.marks,
-        data: { ...state.marks.data, ...action.payload }
-      }
-    }
-
-    case 'marks/unset': return {
-      ...state,
-      marks: {
-        ...state.marks,
-        data: unsetMark(state.marks.data, action.payload)
-      }
-    }
-
-    case 'marks/toggle': return state.marks.data[action.payload.date]
-      ? reducer(state, { type: 'marks/unset', payload: action.payload.date })
-      : reducer(state, { type: 'marks/set', payload: {[action.payload.date]: action.payload.marks} })
-  }
-}
-
-const unsetMark = (marks: Marks, date: string) => Object.entries(marks)
-  .filter(([key]) => key !== date)
-  .reduce((acc, [key, value]) => {
-    acc[key] = value;
-    return acc;
-  }, {} as Marks);
-
-
 export default function CalendarScreen() {
-  const [state, dispatch] = useReducer(reducer, initialArg, init);
-
-
   const store = useStore();
-  const dispatch2 = useDispatch();
+  const dispatch = useDispatch();
 
-  const handleDateClick = useCallback((date: string) => {
-    dispatch2(marksToggle({
+  const handleClick = (date: string) => {
+    dispatch(marksToggle({
       [date]: ['red']
     }));
-  }, [dispatch2]);
+  };
 
   return (
     <>
       <MainPanel year={ store.year } onDispatch={ dispatch } />
-      <main><section><CalendarLocale locale="ru-RU"><YearCalendar year={ store.year } marks={ store.marks } onClick={handleDateClick} /></CalendarLocale></section></main>
+      <main>
+        <section>
+          <CalendarLocale locale="ru-RU">
+            <YearCalendar year={ store.year } marks={ store.marks } onClick={handleClick} />
+          </CalendarLocale>
+        </section>
+      </main>
     </>
   );
 }
