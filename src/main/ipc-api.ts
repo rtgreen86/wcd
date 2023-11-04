c26a199a745a8e9ca6e2c80d2e1e8faffc3fa94f
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2023-11-04 22:13:34 +0100

    export api to render process

--- src/main/ipc-api.ts
+++ src/main/ipc-api.ts
@@ -3,18 +3,7 @@ import * as fs from './models/fs/index';
 
 const key = '00000000000000000000000000000000';
 
-type SavePayload = {
-  name: string,
-  token: string,
-  content: string,
-};
-
-type LoadPayload = {
-  name: string,
-  token: string,
-};
-
 export function handleIpc() {
-  ipcMain.handle('fs-save-user-file', (event, payload: SavePayload) => fs.saveUserFile(payload.name, key, payload.content));
-  ipcMain.handle('fs-load-user-file', (event, payload: LoadPayload) => fs.loadUserFile(payload.name, key));
+  ipcMain.handle('fs-save-user-file', (event, payload: electronAPI.SavePayload) => fs.saveUserFile(payload.name, key, payload.content));
+  ipcMain.handle('fs-load-user-file', (event, payload: electronAPI.LoadPayload) => fs.loadUserFile(payload.name, key));
 }
--- src/preload/preload.d.ts
+++ src/preload/preload.d.ts
@@ -1,8 +1,26 @@
 declare namespace electronAPI {
-    type Request = import('../lib/Request').Request;
-    type Response = import('../lib/Request').Response;
+  type Request = import('../lib/Request').Request;
+  type Response = import('../lib/Request').Response;
 }
 
 declare namespace electronAPI {
-    function request(request: Request): Promise<Response>;
+  function request(request: Request): Promise<Response>;
+}
+
+declare namespace electronAPI {
+  type SavePayload = {
+    name: string,
+    token: string,
+    content: string,
+  };
+
+  type LoadPayload = {
+    name: string,
+    token: string,
+  };
+
+  declare namespace fs {
+    function saveUserFile(payload: SavePayload): Promise<void>
+    function loadUserFile(payload: LoadPayload): Promise<string>
+  }
 }
