c4fdcd4b6c2896280790476253dab77562a9c6e3
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2023-11-25 12:00:30 +0100

    Expand Navbar

--- src/renderer/lib/UIKit/Components/NavBar.tsx
+++ src/renderer/lib/UIKit/Components/NavBar.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export default function NavBar({ children }: Props) {
   return (
-    <nav className="navbar navbar-expand-lg bg-primary">
+    <nav className="navbar navbar-expand bg-primary sticky-top" data-bs-theme="dark">
       <div className="container-fluid">
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
