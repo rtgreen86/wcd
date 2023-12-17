82e882625fc2a367ad1c72541058dcaf628072de
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2023-12-17 12:33:37 +0100

    Delete unused code

--- src/renderer/components/Calendar/Formatter.js
+++
@@ -1,9 +0,0 @@
-export default class Formatter {
-  static isoDate(date) {
-    return [
-      date.getFullYear().toString().padStart(4, '0'),
-      (date.getMonth() + 1).toString().padStart(2, '0'),
-      date.getDate().toString().padStart(2, '0')
-    ].join('-');
-  }
-}
--- src/renderer/components/Calendar/index.js
+++
@@ -1,4 +0,0 @@
-import './styles.css';
-
-export { default as MonthCalendar } from './MonthCalendar';
-export { default as YearCalendar } from './YearCalendar';
