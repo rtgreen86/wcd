f656fee0bc1e1528beddf7b5f0530dbffafc5518
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2024-05-19 21:50:38 +0200

    example bootstrap

--- src/renderer/views/settings-screen.tsx
+++ src/renderer/views/settings-screen.tsx
@@ -20,6 +20,33 @@ export default function SettingsScreen() {
       <BackPanel />
       <main>
         <section>
+          <h1>Modal</h1>
+
+          <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#example-modal">
+            Launch demo modal
+          </button>
+
+          <div className="modal" id="example-modal" tabIndex={-1}>
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title">Modal Title</h5>
+                  <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div className="modal-body">
+                  <p>Modal body text goes here.</p>
+                </div>
+                <div className="modal-footer">
+                  <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" className="btn btn-primary">Save changes</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </section>
+
+        <section>
           <h6>PIN</h6>
           <PinSettings />
         </section>
