a29726b8f0b60e91bb011c5a40d539f8bb3467f7
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2024-05-26 16:16:27 +0200

    remove unused code

--- src/renderer/components/PinSettings.tsx
+++ src/renderer/components/PinSettings.tsx
@@ -64,35 +64,6 @@ export default function PinSettings() {
 
   let message = 'You can protect your application data by settings PIN code.';
 
-  if (isPinExist) {
-    const handleRemovePin = async (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      setLoading(true);
-      setOldPin('');
-      try {
-        const result = await Api.setPin(oldPin, null);
-        setPinExist(!result);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    return (
-      <div>
-        <ModalButton buttonStyle='danger' modalAction="modal-toggle" modalTarget="#delete-pin-modal">Delete PIN</ModalButton>
-        <form onSubmit={handleRemovePin} >
-          <p>
-            Entrer your current PIN code:<br />
-            <label>Current PIN: <PinInput name="pin-code" maxLength={pinSize} onChange={value => setOldPin(value)} /></label>
-          </p>
-          <p><input className='btn' type="submit" value="Delete" /></p>
-        </form>
-      </div>
-    );
-  }
-
   return (
     <section>
       <h5>PIN code</h5>
