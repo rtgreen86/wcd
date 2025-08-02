1328b150bb1a77b270f806c331e4d75c77fa84b5
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2025-08-02 12:40:39 +0200

    Update @vercel/webpack-asset-relocator-loader and fix program start

--- webpack.main.config.ts
+++ webpack.main.config.ts
@@ -1,6 +1,5 @@
 import type { Configuration } from 'webpack';
 import path from 'node:path';
-import { rules } from './webpack.rules';
 
 export const mainConfig: Configuration = {
   /**
@@ -10,7 +9,43 @@ export const mainConfig: Configuration = {
   entry: './src/main/index.ts',
   // Put your normal webpack config below here
   module: {
-    rules,
+    rules: [{
+      // Add support for native node modules
+      // We're specifying native_modules in the test because the asset relocator loader generates a
+      // "fake" .node file which is really a cjs file.
+      test: /native_modules[/\\].+\.node$/,
+      use: 'node-loader',
+    },
+    {
+      test: /[/\\]node_modules[/\\].+\.(m?js|node)$/,
+      parser: { amd: false },
+      use: {
+        loader: '@vercel/webpack-asset-relocator-loader',
+        options: {
+          outputAssetBase: 'native_modules',
+        },
+      },
+    },
+    {
+      test: /\.tsx?$/,
+      exclude: /(node_modules|\.webpack)/,
+      use: {
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+        },
+      },
+    },
+    {
+      test: /\.jsx?$/,
+      exclude: /(node_modules|\.webpack)/,
+      use: {
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+        },
+      },
+    }]
   },
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json'],
--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -1,23 +1,48 @@
 import type { Configuration } from 'webpack';
 import path from 'node:path';
-import { rules } from './webpack.rules';
 import { plugins } from './webpack.plugins';
 
-rules.push({
-  test: /\.css$/,
-  use: [{ loader: 'style-loader', options: { attributes: { nonce: "wcd" } } }, { loader: 'css-loader' }]
-});
+const isPreload = process.env.WEBPACK_RENDERER_TARGET === 'preload'; // <-- Electron Forge так определяет preload
+
+// rules.push({
+//   test: /\.css$/,
+//   use: [{ loader: 'style-loader', options: { attributes: { nonce: "wcd" } } }, { loader: 'css-loader' }]
+// });
+
+console.log('isPreload', isPreload, process.env.WEBPACK_RENDERER_TARGET);
 
 export const rendererConfig: Configuration = {
   module: {
-    rules,
+    rules: [{
+      test: /\.tsx?$/,
+      exclude: /(node_modules|\.webpack)/,
+      use: {
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+        },
+      },
+    },
+    {
+      test: /\.jsx?$/,
+      exclude: /(node_modules|\.webpack)/,
+      use: {
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+        },
+      },
+    },
+    {
+      test: /\.css$/,
+      use: [{ loader: 'style-loader', options: { attributes: { nonce: "wcd" } } }, { loader: 'css-loader' }]
+    }]
   },
   plugins,
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
     alias: {
       "@shared": path.join(__dirname, 'src', 'shared'),
-      "@main": path.join(__dirname, 'src', 'main'),
       "@api": path.join(__dirname, 'src', 'renderer', 'api')
     }
   },
